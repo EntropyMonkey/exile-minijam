2e444f111b393c887a2aef8519295c90017c661c
Andrea Distler <andrea.distler@gmail.com>
2013-10-30 23:03:10 +0100

    added sumoinput, playground scene, gamestate and enums class

---
+++ Unity/Assets/Scripts/GameState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour
+{
+
+	public GameState Instance
+	{
+		get
+		{
+			if (instance == null)
+				instance = this;
+			return instance;
+		}
+	}
+	private GameState instance;
+
+	public int[] PlayerPoints = new int[4];
+
+	// Use this for initialization
+	void Start()
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
